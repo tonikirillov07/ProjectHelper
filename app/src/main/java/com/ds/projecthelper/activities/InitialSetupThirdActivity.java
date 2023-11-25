60dcd3f4e9208b91436f743e2e2369ebaf6f1d10
tonikirillov07 <toni.kirillov.07@inbox.ru>
2023-11-25 17:13:04 +0300

    11/25/2023

--- app/src/main/java/com/ds/projecthelper/activities/InitialSetupThirdActivity.java
+++ app/src/main/java/com/ds/projecthelper/activities/InitialSetupThirdActivity.java
@@ -1,15 +1,24 @@
 package com.ds.projecthelper.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ds.projecthelper.R;
 
 public class InitialSetupThirdActivity extends AppCompatActivity {
+    private Button nextButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_setup_third);
+
+        nextButton = findViewById(R.id.buttonNext);
+        nextButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainPage.class));
+        });
     }
 }
--- app/src/main/java/com/ds/projecthelper/activities/MainPage.java
+++ app/src/main/java/com/ds/projecthelper/activities/MainPage.java
@@ -1,16 +1,29 @@
 package com.ds.projecthelper.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ds.projecthelper.R;
 
 public class MainPage extends AppCompatActivity {
+    private ImageButton mainSettingsButton, mainButton, messengerButton, ideasButton, roadmapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        mainSettingsButton = findViewById(R.id.mainSettingsButton);
+        mainButton = findViewById(R.id.mainButton);
+        messengerButton = findViewById(R.id.messengerButton);
+        ideasButton = findViewById(R.id.ideasButton);
+        roadmapButton = findViewById(R.id.roadmapButton);
+
+        mainSettingsButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainSettingsPage.class));
+        });
     }
 }
